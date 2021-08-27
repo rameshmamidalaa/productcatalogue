da6bda7a5a9bc758e8255bb535cf3189d68afe59
Ramesh Mamidala <41686542+rameshmamidalaa@users.noreply.github.com>
2021-08-27 19:09:16 +0200

    Correct right http status code.

--- src/main/java/com/snippers/hackathon/controller/ProductCategoryController.java
+++ src/main/java/com/snippers/hackathon/controller/ProductCategoryController.java
@@ -47,7 +47,7 @@ public class ProductCategoryController {
 			@ApiResponse(code = 200, message = "Successfully retrieved the list"),
 			@ApiResponse(code = 401, message = "You are not authorized to view the resource"),
 			@ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-			@ApiResponse(code = 401, message = "The resource you were trying to reach is not found")
+			@ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
 	})
 	@GetMapping("/productCategories")
 	public ResponseEntity<List<ProductCategoryDTO>> getAllProductCategories() {
